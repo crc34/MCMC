5fd7158849fd19d6b3a43bc0dcce64e65391016e
user <crcastore@gmail.com>
2015-05-25 15:50:51 -0400

    add runName field to run table and delete tables if they exist

--- master/DatabaseSchemas/oneParam.sql
+++ master/DatabaseSchemas/oneParam.sql
@@ -1,10 +1,13 @@
 DROP TABLE IF EXISTS samples;
 
+DROP TABLE IF EXISTS run;
 CREATE TABLE run (
     runId INT,
+    runName varchar(255),
     params blob
 );
 
+DROP TABLE IF EXISTS samples;
 CREATE TABLE samples (
     iteration INT KEY AUTO_INCREMENT,
     runId INT,
