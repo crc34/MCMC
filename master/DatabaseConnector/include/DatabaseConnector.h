7667fdfea6ed8aa215236ba8abac9fc294c71f04
crc34 <crcastore@gmail.com>
2015-08-23 16:08:47 -0400

    add in some comments

--- master/DatabaseConnector/include/DatabaseConnector.h
+++ master/DatabaseConnector/include/DatabaseConnector.h
@@ -24,11 +24,23 @@ class DatabaseConnector
         *     \param userPassword password.` 
         *     \param database databaseName.`   
         **/ 
-        DatabaseConnector(const std::string hostName,
-            const std::string userName, const std::string userPassword,
+        DatabaseConnector(
+            const std::string hostName,
+            const std::string userName,
+            const std::string userPassword,
             const std::string database);
+        /*! execute the input query. Only use for query with no return values.
+         *    \param query query string
+         **/
         bool execute(const std::string query);
+
+        /*! execute the input query. Use when you want results returned.
+         *    \param query query string
+         *    \return shared pointer to the result set.
+         **/
         std::shared_ptr<sql::ResultSet> executeFetchQuery(std::string query);
+
+        /** returns true if the connector is connected*/ 
         bool isConnected(){return m_connected;}
     private:
         bool connect(const std::string hostName, const std::string userName,
