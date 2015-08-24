3453faedec9ba8af29505ff4ca363dabd72e7945
crc34 <crcastore@gmail.com>
2015-08-24 18:31:52 -0400

    cleanup

--- master/DatabaseConnector/src/DatabaseConnector.cpp
+++ master/DatabaseConnector/src/DatabaseConnector.cpp
@@ -1,20 +1,27 @@
 #include "DatabaseConnector.h"
 #include <sstream>
 
-DatabaseConnector::DatabaseConnector(std::string hostName, std::string userName,
-        std::string userPassword, std::string database) :
-m_driver(get_driver_instance()) {
+DatabaseConnector::DatabaseConnector(
+        std::string hostName,
+        std::string userName,
+        std::string userPassword,
+        std::string database) :
+        m_driver(get_driver_instance())
+{
     m_connected = connect(hostName, userName, userPassword, database);
 }
 
-DatabaseConnector::~DatabaseConnector() {
-}
+DatabaseConnector::~DatabaseConnector() {}
 
-bool DatabaseConnector::connect(const std::string hostName,
-        const std::string userName, const std::string userPassword,
-        const std::string database) {
+bool DatabaseConnector::connect(
+    const std::string hostName,
+    const std::string userName,
+    const std::string userPassword,
+    const std::string database)
+{
     m_connection = m_driver->connect(hostName, userName, userPassword);
-    std::stringstream useQuery; // Use the right database
+    // Use the right database
+    std::stringstream useQuery; 
     useQuery << "USE " << database;
     return execute(useQuery.str());
 }
@@ -24,9 +31,10 @@ bool DatabaseConnector::execute(const std::string query) {
     return stmt->execute(query);
 }
 
-std::shared_ptr<sql::ResultSet> DatabaseConnector::executeFetchQuery(std::string query) {
-    sql::Statement *stmt = m_connection->createStatement();
-    sql::ResultSet* result = stmt->executeQuery(query);
+std::shared_ptr<sql::ResultSet> DatabaseConnector::executeFetchQuery(std::string query)
+{
+    auto stmt = m_connection->createStatement();
+    auto result = stmt->executeQuery(query);
     std::shared_ptr<sql::ResultSet> resultPointer;
     resultPointer.reset(result);
     return resultPointer;
