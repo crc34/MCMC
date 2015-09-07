dc52ddbf1f63a6e0ca17144dc10129f173a62469
user <user@localhost.localdomain>
2015-09-07 15:43:44 -0400

    cleanup

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -6,15 +6,17 @@
 #include <mpi.h>
 using namespace cv;
 
-int n = 2000;
+int n = 30000;
 double p = 0.05;
+
+int nElementsPerProcess;
+int leftOverElements;
 
 Size size(1000, 1000);
 int rank;
 char hostname[256];
 MPI_Status* status;
 int nProcesses;
-
 enum class TAGS {BITS}; 
 
 std::shared_ptr<Mat> readImage()
@@ -35,7 +37,7 @@ bool draw(unsigned char * bits, int startIndex, int endIndex)
         auto draw =  ((double) rand() / ((double)RAND_MAX));
         if(draw > p)
 		{
-			bits[i] = 255*draw;
+			bits[i] = rank*draw*255;
 		}
     }
     return true;
@@ -50,6 +52,46 @@ bool displayImage(const Mat image)
     return false;
 }
 
+bool displayImageFromCharArray(unsigned char* bits)
+{
+        Mat image(n, n, CV_8UC1, bits);
+        //resize(image, image, size);
+        //displayImage(image);
+        return true;
+}
+
+bool processSlaves()
+{
+        unsigned char* bits =  (unsigned char *)calloc(nElementsPerProcess, sizeof(unsigned char));
+        MPI_Recv(bits, nElementsPerProcess, MPI_UNSIGNED_CHAR, 0, static_cast<int>(TAGS::BITS), MPI_COMM_WORLD, status);
+        draw(bits, 0, nElementsPerProcess);
+        auto retVal = MPI_Send( &bits[0], nElementsPerProcess, MPI_UNSIGNED_CHAR, 0, static_cast<int>(TAGS::BITS), MPI_COMM_WORLD);
+        delete(bits);
+}
+
+bool delegateBits(unsigned char* bits)
+{
+    for (int i = 1; i < nProcesses; i++)
+    {
+        unsigned char* startChar = &bits[(i-1)*nElementsPerProcess];
+        auto retVal =
+            MPI_Send(startChar, nElementsPerProcess, MPI_UNSIGNED_CHAR, i, static_cast<int>(TAGS::BITS), MPI_COMM_WORLD);
+    }
+    for (int i = 1; i < nProcesses; i++)
+    {
+        unsigned char* startChar = &bits[(i-1)*nElementsPerProcess];
+        MPI_Recv(startChar, nElementsPerProcess, MPI_UNSIGNED_CHAR, i, static_cast<int>(TAGS::BITS), MPI_COMM_WORLD, status);
+    }
+}
+
+bool processMaster()
+{
+        unsigned char* bits =  (unsigned char *)calloc(n*n, sizeof(unsigned char));
+        delegateBits(bits);
+        displayImageFromCharArray(bits);  
+        delete(bits);
+        return true;
+}
 
 int main(int argc, char** argv )
 {
@@ -57,23 +99,16 @@ int main(int argc, char** argv )
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     gethostname(hostname, 255);
     MPI_Comm_size(MPI_COMM_WORLD, &nProcesses);
+    nElementsPerProcess = (n*n)/(nProcesses-1);
+    leftOverElements = (n*n) % nProcesses;
+ 
     if (rank == 0)
     {
-        unsigned char* bits =  (unsigned char *)calloc(n*n, sizeof(unsigned char));
-        auto retVal = MPI_Send( &bits[0], n*n, MPI_UNSIGNED_CHAR, 1, static_cast<int>(TAGS::BITS), MPI_COMM_WORLD  );
-        MPI_Recv(bits, n*n, MPI_UNSIGNED_CHAR, 1, static_cast<int>(TAGS::BITS), MPI_COMM_WORLD, status);
-        Mat image(n, n, CV_8UC1, bits);
-        resize(image, image, size);
-        displayImage(image);
-        delete(bits);
-        return 0;
+        processMaster();
     }
     else
     {
-        unsigned char* bits =  (unsigned char *)calloc(n*n, sizeof(unsigned char));
-        MPI_Recv(bits, n*n, MPI_UNSIGNED_CHAR, 0, static_cast<int>(TAGS::BITS), MPI_COMM_WORLD, status);
-        draw(bits, 0, n*n);
-        auto retVal = MPI_Send( &bits[0], n*n, MPI_UNSIGNED_CHAR, 0, static_cast<int>(TAGS::BITS), MPI_COMM_WORLD);
+        processSlaves();
     }
     MPI_Finalize();
 }
